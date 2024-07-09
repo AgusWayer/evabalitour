3844367ddce644daa0146d4584f9deec5fefb376
AgusWayer <aguswira246@gmail.com>
2024-07-09 10:07:23 +0800

    first commit

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,31 @@
+<nav class="navbar navbar-expand-md bg-transparent">
+    <div class=" offcanvas offcanvas-start" id="navbar">
+        <div class="offcanvas-header">
+            <a href="" class="navbar-brand">Eva Bali Tours</a>
+            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        <div class="offcanvas-body">
+            <div class="container-md menu-container">
+                <a href="/" class="navbar-brand d-none d-md-block">Eva Bali Tours</a>
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a href="/" class="nav-link">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="/about-us" class="nav-link">About Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="/packages" class="nav-link">Our Packages</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="/packages" class="nav-link">Destinations</a>
+                    </li>
+                </ul>
+                <a href="" class="btn ">Book Now</a>
+            </div>
+        </div>
+    </div>
+    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#navbar">
+        <i class="fa-solid fa-bars-staggered"></i>
+    </button>
+</nav>
