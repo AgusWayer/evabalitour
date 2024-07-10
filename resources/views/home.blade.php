bdbe7c2cfa6abd649c654dace520b9ab629c9d38
AgusWayer <aguswira246@gmail.com>
2024-07-10 10:39:15 +0800

    add value

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,9 @@
 @section('content')
     <div class="container-fluid hero-section m-0 p-0">
         <div class="container content text-center text-md-start vh-100 text-white">
+            {{-- img overlay  --}}
             <img src="{{asset('img/img-hero.webp')}}" class="image-bg w-100 h-100" alt="">
+            {{-- img overlay end --}}
             <div class="row h-100 d-flex flex-column justify-content-center">
                 <section class="col-md-6">
                     <h1 class="header-hero">EXPLORE BALI</h1>
@@ -13,8 +15,9 @@
             </div>
         </div>
     </div>
-    <div class="container">
-        <section class="floating-input">
+    <div class="container position-relative">
+        {{-- floating input --}}
+        <section class="floating-input w-100">
             <form action="" class="row align-items-center  form ">
                 <div class="text-center col-md-2">
                     <label for="" class="d-block">From *</label>
@@ -33,9 +36,48 @@
                     <input type="text" class="form-control" name="time">
                 </div>
                 <div class="text-center col-md-2 h-100 m-0 p-0">
-                    <button class="book-btn d-block ">BOOK NOW</button>
+                    <button class="book-btn d-block fw-bold fs-5">BOOK NOW</button>
                 </div>
             </form>
         </section>
+        {{-- floating input end --}}
+
+        {{-- value --}}
+        <section class="container">
+            <h5>Why Choose Us?</h5>
+            <p>Try Variety of Benefits when using our services</p>
+            <div class="row">
+                <section class="col-md-3 text-center">
+                    <div class="icon-container fs-2 rounded-circle mx-auto text-white m-0 p-0">
+                        <i class="fa-regular fa-paper-plane"></i>
+                    </div>
+                    <h5 class="fw-bold text-orange my-2">Airport Pickup</h5>
+                    <small>Enjoy seamless and stress- transportation with our airport pickup service.</small>
+                </section>
+                <section class="col-md-3 text-center">
+                    <div class="icon-container fs-2 rounded-circle mx-auto text-white m-0 p-0">
+                        <i class="fa-regular fa-calendar-check"></i>
+                    </div>
+                    <h5 class="fw-bold text-orange my-2">Easy Booking </h5>
+                    <small>Enjoy seamless and stress- transportation with our airport pickup service.</small>
+                </section>
+                <section class="col-md-3 text-center">
+                    <div class="icon-container fs-2 rounded-circle mx-auto text-white m-0 p-0">
+                        <i class="fa-solid fa-star"></i>
+                    </div>
+                    <h5 class="fw-bold text-orange my-2">Best Tour Guide</h5>
+                    <small>Enjoy seamless and stress- transportation with our airport pickup service.</small>
+                </section>
+                <section class="col-md-3 text-center">
+                    <div class="icon-container fs-2 rounded-circle mx-auto text-white m-0 p-0">
+                        <i class="fa-solid fa-face-grin-wink"></i>
+                    </div>
+                    <h5 class="fw-bold text-orange my-2">Happy Tour</h5>
+                    <small>Enjoy seamless and stress- transportation with our airport pickup service.</small>
+                </section>
+            </div>
+        </section>
+        {{-- valueend --}}
+
     </div>
 @endsection
