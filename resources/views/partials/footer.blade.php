fcb739765d74fba4b5beb6306a61a8b0a4ec5c8d
AgusWayer <aguswira246@gmail.com>
2024-07-17 09:52:44 +0800

    save fifle?

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,18 @@
+<footer class="container-fluid bg-orange footer py-3">
+    <div class="container">
+        <div class="row">
+            <section class="col-md-4">
+                <h4>Eva Bali Tours</h4>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid repellat consectetur beatae veritatis nisi perferendis quibusdam nemo facilis consequatur saepe?</p>
+            </section>
+            <div class="col-md-4">
+                <h4>Navigations</h4>
+                <ul class="navbar-nav">
+                    <li class=""><a href="" class="nav-link">Home</a></li>
+                    <li class=""><a href="" class="nav-link">Home</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+</footer>
